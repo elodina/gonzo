efc6ba1de81d926925138e0d860d6aca17f2b5a9
Alexey Grachov <grachov.alexey@gmail.com>
2016-02-09 13:03:29 +0200

    Fix collectorFunc to return error.

--- partition_consumer.go
+++ partition_consumer.go
@@ -265,17 +265,19 @@ func (pc *KafkaPartitionConsumer) resetOffset() bool {
 	}
 }
 
-func (pc *KafkaPartitionConsumer) collectorFunc(messages *[]*MessageAndMetadata) func(topic string, partition int32, offset int64, key []byte, value []byte) {
-	return func(topic string, partition int32, offset int64, key []byte, value []byte) {
+func (pc *KafkaPartitionConsumer) collectorFunc(messages *[]*MessageAndMetadata) func(topic string, partition int32, offset int64, key []byte, value []byte) error {
+	return func(topic string, partition int32, offset int64, key []byte, value []byte) error {
 		decodedKey, err := pc.config.KeyDecoder.Decode(key)
 		if err != nil {
 			//TODO siesta should support collector function to return an error
 			Logger.Warn(err.Error())
+			return err
 		}
 		decodedValue, err := pc.config.ValueDecoder.Decode(value)
 		if err != nil {
 			//TODO siesta should support collector function to return an error
 			Logger.Warn(err.Error())
+			return err
 		}
 
 		*messages = append(*messages, &MessageAndMetadata{
@@ -287,6 +289,7 @@ func (pc *KafkaPartitionConsumer) collectorFunc(messages *[]*MessageAndMetadata)
 			DecodedKey:   decodedKey,
 			DecodedValue: decodedValue,
 		})
+		return nil
 	}
 }
 
